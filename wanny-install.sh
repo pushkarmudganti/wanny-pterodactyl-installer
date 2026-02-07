f32f216ab684cffc4ee236cf6864ece8c32c6b42
WannyDragon <pushkarmudganti@gmail.com>
2026-02-07 22:48:25 +0530

    Delete wanny-install.sh

--- wanny-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# WANNY Pterodactyl Installer - One-liner version
-echo "🚀 Installing WANNY Pterodactyl Installer..."
-
-# Download and run the full installer
-bash <(curl -s https://raw.githubusercontent.com/pushkarmudganti/wanny-pterodactyl-installer/main/installer.sh)
